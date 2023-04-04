b63a7f500b4d7bed0c0b971e686e9bf87e70d960
RupanjaliKukal <100168534+RupanjaliKukal@users.noreply.github.com>
2023-04-04 19:09:21 -0400

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,8 +1,9 @@
 from utils import show_tiff
 from glob import glob 
+import os 
 
 #change the path according to requirement 
-tif_path = "./data/training.tif"
+tif_path = "./data/testing.tif"
 output_path = "./data/visualize/images/"
-
+os.makedirs(output_path, exist_ok =True)
 show_tiff(tif_path, output_path)
